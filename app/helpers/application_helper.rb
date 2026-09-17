ddf1264846fc28e8d9b0e0cf21fb4f118c364cab
Mikita <mikit.sazan@gmail.com>
2026-09-18 01:08:43 +0300

    Design layout: header, main area, footer and flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def alert_classes(type)
+    case type.to_s
+    when "success"
+      "bg-green-50 text-green-700"
+    when "error", "alert"
+      "bg-red-50 text-red-700"
+    else
+      "bg-blue-50 text-blue-700"
+    end
+  end
 end
